2e68a2ae07ec3ca83616c1278b0e2a416884617c
Faiza Husain <faiza.ahsan1222@gmail.com>
2018-06-27 10:41:59 -0700

    Pushing all current Eclipse projects to GitHub

---
+++ src/MyInitials.java
@@ -0,0 +1,13 @@
+
+public class MyInitials {
+	public static void main(String[] args) {
+		System.out.println("FFFFFFFFFF          A");
+		System.out.println("F                  A A");
+		System.out.println("F                 A   A");
+		System.out.println("F                A     A");	
+		System.out.println("FFFFF           AAAAAAAAA");
+		System.out.println("F              A         A");
+		System.out.println("F             A           A");
+		System.out.println("F            A             A");
+	}
+}
