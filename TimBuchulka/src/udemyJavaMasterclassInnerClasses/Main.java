8aeb3383e0be57f8cf450e9ef537afbb894392df
Faiza Ahsan <faiza.ahsan1222@gmail.com>
2020-09-09 00:33:03 -0700

    Inner Classes

---
+++ TimBuchulka/src/udemyJavaMasterclassInnerClasses/Main.java
@@ -0,0 +1,72 @@
+package udemyJavaMasterclassInnerClasses;
+
+import java.util.Scanner;
+
+public class Main {
+	private static Scanner scanner = new Scanner(System.in);
+	private static Button btnPrint = new Button("Print");
+	
+	public static void main(String[] args) {
+//		Gearbox mcLaren = new Gearbox(6);
+//
+//		mcLaren.operateClutch(true);
+//		mcLaren.changeGear(1);
+//		mcLaren.operateClutch(false);
+//		System.out.println(mcLaren.wheelSpeed(1000));
+//		mcLaren.changeGear(2);
+//		System.out.println(mcLaren.wheelSpeed(3000));
+//		mcLaren.operateClutch(true);
+//		mcLaren.changeGear(3);
+//		mcLaren.operateClutch(false);
+//		System.out.println(mcLaren.wheelSpeed(6000));
+		
+//		// Local class
+//		class ClickListener implements Button.OnClickListener {
+//			public ClickListener() {
+//				System.out.println("I have been attached.");
+//			}
+//			
+//			@Override
+//			public void onClick(String title) {
+//				System.out.println(title + " was clicked.");
+//			}		
+//		}
+//		
+//		btnPrint.setOnClickListener(new ClickListener());
+//		listen();
+		
+//		// Anonymous
+		btnPrint.setOnClickListener(new Button.OnClickListener(){
+			public ClickListener() {
+				System.out.println("I have been attached.");
+			}
+			@Override
+			public void onClick(String title) {
+				public void onClick(String title) {
+					System.out.println(title + " was clicked.");
+				}
+			}
+		});
+		listen();
+	}
+	
+	private static void listen() {
+		boolean quit = false;
+		while(!quit) {
+			int choice = scanner.nextInt();
+			scanner.nextLine();
+			switch(choice) {
+				case 0: quit = true; break;
+				case 1: btnPrint.onClick(); break;
+			}
+		}
+	}
+}
+
+
+//Outerclass.Innerclass
+//Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+
+// Invalid instantiations
+//Gearbox.Gear second = new Gearbox.Gear(2, 15.4);
+//Gearbox.Gear third = new mcLaren.Gear(3, 17.8);
--- TimBuchulka/src/udemyJavaMasterclassLinkedList/Challenge/Album.java
+++ TimBuchulka/src/udemyJavaMasterclassLinkedList/Challenge/Album.java
@@ -15,39 +15,61 @@ public class Album {
 	}
 	
 	public boolean addSong(String title, double duration) {
-		if(findSong(title) == null) {
-			this.songs.add(new Song(title, duration));
-			return true;
-		}
-		return false;
+		return this.songs.add(new Song(title, duration));
 	}	
 	
-	private Song findSong(String title) {
-		for(Song song : this.songs) {
-			if(song.getTitle().equalsIgnoreCase(title)){
-				return song;
-			}
-		}
-		return null;
-	}
-	
 	public boolean addToPlaylist(int trackNumber, LinkedList<Song> playlist) {
-		int index = trackNumber - 1;
-		if(index >= 0 && index <= this.songs.size()) {
-			playlist.add(this.songs.get(index));
+		Song checkedSong = this.songs.findSong(trackNumber);
+		if(checkedSong != null) {
+			playlist.add(checkedSong);
 			return true;
 		}
+		
 		System.out.println("This album does not have a track: " + trackNumber);
 		return false;
 	}
 	
 	public boolean addToPlaylist(String title, LinkedList<Song> playlist) {
-		Song song = findSong(title);
+		Song song = songs.findSong(title);
 		if(song != null) {
 			playlist.add(song);
 			return true;
 		}
 		System.out.println("This album does not have a track with title: " + title);
 		return false;
+	}
+	
+	private class SongList {
+		private ArrayList<Song> songs;
+		
+		public SongList() {
+			this.songs = new ArrayList<Song>();
+		}
+		
+		private boolean addSong(Song song) {
+			if(songs.contains(song)) {
+				return false;
+			}
+			this.songs.add(song);
+			return true;
+		}
+		
+		
+		private Song findSong(String title) {
+			for(Song song : this.songs) {
+				if(song.getTitle().equalsIgnoreCase(title)){
+					return song;
+				}
+			}
+			return null;
+		}
+		
+		public Song findSong(int trackNumber) {
+			int index = trackNumber - 1;
+			if(index > 0 && index <= songs.size()) {
+				return songs.get(index);
+			}
+			return null;
+		}
 	}
 }
