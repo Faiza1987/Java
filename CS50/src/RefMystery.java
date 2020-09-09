c71adceefb0ed54024668a2a65621a0725c7b12d
Faiza Ahsan <faiza.ahsan1222@gmail.com>
2020-09-09 00:35:09 -0700

    Inner Classes

---
+++ CS50/src/RefMystery.java
@@ -0,0 +1,13 @@
+
+public class RefMystery {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int[] list = new int[2];
+		
+		System.out.println(x);
+		System.out.println(list[x]++);
+
+	}
+
+}
